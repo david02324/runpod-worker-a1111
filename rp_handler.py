50bea6fa86e82139a214273b24ce8d3b5cc1ac7f
jangdawit <jangdawit@gmail.com>
2023-09-09 06:26:31 +0900

    v1.1.6

--- rp_handler.py
+++ rp_handler.py
@@ -1,6 +1,10 @@
 import time
 import requests
 import runpod
+import threading
+import redis
+import json
+import os
 from runpod.serverless.utils.rp_validator import validate
 from runpod.serverless.modules.rp_logger import RunPodLogger
 from requests.adapters import HTTPAdapter, Retry
@@ -10,12 +14,14 @@ from schemas.txt2img import TXT2IMG_SCHEMA
 from schemas.options import OPTIONS_SCHEMA
 
 BASE_URL = 'http://127.0.0.1:3000'
+REDIS_URL= os.environ.get('REDIS_URL')
 TIMEOUT = 600
 
 session = requests.Session()
 retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
 session.mount('http://', HTTPAdapter(max_retries=retries))
 logger = RunPodLogger()
+redis_client = redis.from_url(url=REDIS_URL)
 
 
 # ---------------------------------------------------------------------------- #
@@ -40,6 +46,30 @@ def wait_for_service(url):
 
         time.sleep(0.2)
 
+task_map = {}
+def progress_T2I(event_id):
+    previous_preview = ''
+    while task_map[event_id]:
+        time.sleep(1)
+        res = requests.get(f'{BASE_URL}/sdapi/v1/progress')
+        res_json = res.json()
+
+        packet = {}
+        packet['id'] = event_id
+        data = {}
+        if 'progress' in res_json and res_json['progress'] != None:
+            data['progress'] = res_json['progress']
+        if 'current_image' in res_json and res_json['current_image'] != None and previous_preview != res_json['current_image']:
+            previous_preview = res_json['current_image']
+            data['preview'] = 'data:image/png;base64,' + previous_preview
+
+        packet['data'] = data
+
+        channel = 'progress'
+        message = json.dumps(packet)
+
+        redis_client.publish(channel=channel, message=message)
+
 
 def send_get_request(endpoint):
     return session.get(
@@ -48,12 +78,20 @@ def send_get_request(endpoint):
     )
 
 
-def send_post_request(endpoint, payload):
-    return session.post(
+def send_post_request(endpoint, payload, event_id):
+    if endpoint == 'sdapi/v1/txt2img':
+        non_blocking_thread = threading.Thread(target=progress_T2I, args=(event_id,))
+        task_map[event_id] = True
+        non_blocking_thread.start()
+
+    res = session.post(
         url=f'{BASE_URL}/{endpoint}',
         json=payload,
         timeout=TIMEOUT
     )
+    task_map[event_id] = False
+
+    return res
 
 
 def validate_api(event):
@@ -77,6 +115,12 @@ def validate_api(event):
 def validate_payload(event):
     method = event['input']['api']['method']
     endpoint = event['input']['api']['endpoint']
+    subModels = []
+    if 'subModels' in event['input']:
+        subModels = event['input']['subModels']
+    else:
+        logger.log('No subModels')
+
     payload = event['input']['payload']
     validated_input = payload
 
@@ -87,13 +131,14 @@ def validate_payload(event):
     elif endpoint == 'options' and method == 'POST':
         validated_input = validate(payload, OPTIONS_SCHEMA)
 
-    return endpoint, event['input']['api']['method'], validated_input
+    return endpoint, event['input']['api']['method'], validated_input, subModels
 
 
 # ---------------------------------------------------------------------------- #
 #                                RunPod Handler                                #
 # ---------------------------------------------------------------------------- #
 def handler(event):
+    event_id = event['id']
     validated_api = validate_api(event)
 
     if 'errors' in validated_api:
@@ -101,7 +146,7 @@ def handler(event):
             'error': validated_api['errors']
         }
 
-    endpoint, method, validated_input = validate_payload(event)
+    endpoint, method, validated_input, subModels = validate_payload(event)
 
     if 'errors' in validated_input:
         return {
@@ -113,13 +158,40 @@ def handler(event):
     else:
         payload = validated_input
 
+
+    try:
+        shouldRefreshLora = False
+        for subModel in subModels:
+            name = subModel['name']
+            url = subModel['url']
+            t = subModel['t']
+            logger.log(f'Downloading {t}/{name} from: {url}')
+
+            r = requests.get(url)
+
+            if t == 'TI':
+                p = f'/workspace/stable-diffusion-webui/embeddings/{name}'
+                open(p, 'wb').write(r.content)
+            elif t == 'LORA':
+                shouldRefreshLora = True
+                p = f'/workspace/stable-diffusion-webui/models/Lora/{name}'
+                open(p, 'wb').write(r.content)
+
+        if shouldRefreshLora:
+            send_post_request('sdapi/v1/refresh-loras', {}, '')
+
+    except Exception as e:
+        return {
+            'error': str(e)
+        }
+
     try:
         logger.log(f'Sending {method} request to: /{endpoint}')
 
         if method == 'GET':
             response = send_get_request(endpoint)
         elif method == 'POST':
-            response = send_post_request(endpoint, payload)
+            response = send_post_request(endpoint, payload, event_id)
     except Exception as e:
         return {
             'error': str(e)
